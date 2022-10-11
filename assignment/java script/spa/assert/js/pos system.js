ac698230fff74ac554b934efed62c187055e69a6
ALE IS TER <lakshanherath1101@gmail.com>
2022-10-10 20:23:43 -0700

    order form complete and navigated

--- assignment/java script/spa/assert/js/pos system.js
+++ assignment/java script/spa/assert/js/pos system.js
@@ -1,7 +1,7 @@
 $('#submit_btn').click(function () {
   let username = $('#username').val();
   let password = $('#password').val();
-  if (username=="pathum" && password=="1234"){
+  if (username==="pathum" && password==="1234"){
     $('#lord_for_dashboard').load('dashboard.html');
   }else {
     $('#exampleModalCenter').modal('show');
